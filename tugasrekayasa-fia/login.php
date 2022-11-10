0238aa58b7668bf2c92c45c99ad58470412d010f
Nur Afifah Putri Alifia <82718740+nurafifa@users.noreply.github.com>
2022-11-10 21:46:34 +0700

    Add files via upload

---
+++ tugasrekayasa-fia/login.php
@@ -0,0 +1,48 @@
+<?php
+// Cek apakah tombol submit sudah di tekan apa belum
+if ( isset($_POST["submit"])) {
+// Cek Username dan Password
+    if( $_POST["username"] == "fia" && $_POST["password"]== "fia") {
+// Jika Benar, redirect ke halaman admin
+    header("Location:fia.html");
+    exit;    
+}  else {
+// Jika salah, tampilkan pesan kesalahan
+    $error = true;
+    
+        }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Login</title>
+</head>
+<body>
+    <h1> Login Admin</h1>
+    <?php if( isset($error)) :?>
+    <p style= "color: red; font-style: ityalic" > Username/Password salah!</p>
+    <?php endif; ?>
+
+    <ul>
+    <form action="" method="post">
+        <li>
+            <label for="username">Username:</label>
+            <input type="text" name=username id="username">
+        </li>
+        <li>
+            <label for="password">Password:</label>
+            <input type="password" name=password id="password">
+        </li>
+        <li>
+            <button type="submit" name="submit">Login</button>
+        </li>
+    </form>
+    </ul>
+</body>
+</html>
